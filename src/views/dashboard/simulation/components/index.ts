d7562c45d04f5d8697f37700dacec2448a247e2e
ccchhhddd <121497181+ccchhhddd@users.noreply.github.com>
2023-10-08 07:49:58 +0800

    Delete src/views/dashboard directory

--- src/views/dashboard/simulation/components/index.ts
+++
@@ -1,6 +0,0 @@
-import TopChart from './top-chart/index.vue';
-import DataCard from './data-card/index.vue';
-import BottomPart from './bottom-part/index.vue';
-import ModeChoice from './mode-choice/index.vue';
-
-export { TopChart, DataCard, BottomPart, ModeChoice};
--- src/views/dashboard/workbench/components/index.ts
+++
@@ -1,4 +0,0 @@
-import WorkbenchHeader from './workbench-header/index.vue';
-import WorkbenchMain from './workbench-main/index.vue';
-import ModeChoice from './mode-choice/index.vue';
-export { WorkbenchHeader, WorkbenchMain, ModeChoice };
