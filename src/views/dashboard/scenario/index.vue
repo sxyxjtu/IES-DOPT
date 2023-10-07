c36de33012425fead5fee0f2bd56cd354b004fb9
ccchhhddd <121497181+ccchhhddd@users.noreply.github.com>
2023-10-08 07:52:33 +0800

    Add files via upload

---
+++ src/views/dashboard/scenario/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <n-space :vertical="true" :size="16">
+    <mode-choice :title="''"/>
+    <!-- <top-chart />
+    <data-card />
+    <bottom-part /> -->
+  </n-space>
+</template>
+
+<script lang="ts" setup>
+import { ModeChoice } from './components';
+</script>
+
+<style scoped></style>
